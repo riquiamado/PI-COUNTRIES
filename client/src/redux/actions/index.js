157cb7607d98626c23e8cbcbfab445747b43a431
riquiamado <carlosamado221471@gmail.com>
2023-03-17 19:42:44 -0300

    dep

--- client/src/redux/actions/index.js
+++ client/src/redux/actions/index.js
@@ -12,57 +12,51 @@ import {
   GET_COUNTRY_BYNAME,
   CLEAN,
   SET_PAGE,
-  DELETE_ACTIVITY
- 
+  DELETE_ACTIVITY,
 } from "./componentes";
 
+const URL = `pi-countries-production-4c0e.up.railway.app`;
+//  const URL= `http://localhost:3001`
 export function getAllCountries() {
-  return  function (dispatch) {
-     axios.get(`http://localhost:3001/countries`)
-     .then(response => response.data)
-     .then(data => dispatch({ type: GET_ALL_COUNTRIES, payload:data }))
-         
+  return function (dispatch) {
+    axios
+      .get(`${URL}/countries`)
+      .then((response) => response.data)
+      .then((data) => dispatch({ type: GET_ALL_COUNTRIES, payload: data }));
   };
 }
 
-
-export function  getCountriesByName(name){
-  return  async function(dispatch){
+export function getCountriesByName(name) {
+  return async function (dispatch) {
     try {
-       const res= await  axios.get(`http://localhost:3001/countries?name=${name}`)
-    
-    dispatch({type:GET_COUNTRY_BYNAME,  payload:res.data})
-    } catch (error) {
-     alert("No existe este pais sigue intentando")
-    }
-    
-      
-    } 
-  }
-  
+      const res = await axios.get(`${URL}/countries?name=${name}`);
 
+      dispatch({ type: GET_COUNTRY_BYNAME, payload: res.data });
+    } catch (error) {
+      alert("No existe este pais sigue intentando");
+    }
+  };
+}
 
 export function getActivities() {
   return async function (dispatch) {
     try {
-      const info = await axios.get(`http://localhost:3001/activities`);
+      const info = await axios.get(`${URL}/activities`);
       return dispatch({ type: GET_ACTIVITIES, payload: info.data });
     } catch (error) {
       console.log(error);
     }
-   
   };
 }
 
 export function getCountriesDetails(id) {
   return async function (dispatch) {
     try {
-      const json = await axios.get(`http://localhost:3001/countries/${id}`);
-    return dispatch({ type: GET_COUNTRIES_DETAILS, payload: json.data });
+      const json = await axios.get(`${URL}/countries/${id}`);
+      return dispatch({ type: GET_COUNTRIES_DETAILS, payload: json.data });
     } catch (error) {
       console.log(error);
     }
-    
   };
 }
 
@@ -95,43 +89,39 @@ export function orderByPopulation(payload) {
 }
 
 export function deleteActivity(id) {
-  return async function (dispatch) { 
-     try{ 
-      const activity = await axios.delete(`http://localhost:3001/activities/${id}`)
-      return dispatch({ 
-        type: DELETE_ACTIVITY, 
-        payload: activity, 
+  return async function (dispatch) {
+    try {
+      const activity = await axios.delete(`${URL}/activities/${id}`);
+      return dispatch({
+        type: DELETE_ACTIVITY,
+        payload: activity,
       });
-     } catch (error){
-      alert(error,"daleee")
-     }
+    } catch (error) {
+      alert(error, "daleee");
+    }
   };
-};
+}
 // export function deleteActivity(payload){
 //   return function (dispatch){
-   
+
 //     dispatch({type:DELETE_ACTIVITY, payload:payload})
 //   }
-  
-// }
 
+// }
 
 export function addActivities(payload) {
   return async function (dispatch) {
     try {
-      let info = await axios.post("http://localhost:3001/activities", payload);
-      
+      let info = await axios.post(`${URL}/activities`, payload);
+
       return dispatch({
         type: ADD_ACTIVITIES,
         payload: info.data,
-        
       });
-      
     } catch (error) {
-       alert("esta actividad ya fue creada")
-  };
+      alert("esta actividad ya fue creada");
     }
-   
+  };
 }
 export function setCurrentPage(page) {
   return {
