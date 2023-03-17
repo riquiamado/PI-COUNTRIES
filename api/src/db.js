322105c34b5444e146f2dd1d6104d84e1bc9e4d6
riquiamado <carlosamado221471@gmail.com>
2023-03-17 19:38:58 -0300

    deploy

--- api/src/db.js
+++ api/src/db.js
@@ -6,6 +6,7 @@ const { DB_USER, DB_PASSWORD, DB_HOST } = process.env;
 
 const sequelize = new Sequelize(
   `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/railway`,
+  // `postgresql://postgres:tk4vEHesoGw6YOulP0V4@containers-us-west-89.railway.app:7468/railway`,
   {
     logging: false, // set to console.log to see the raw SQL queries
     native: false, // lets Sequelize know we can use pg-native for ~30% more speed
