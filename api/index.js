05f98e978a940e754b3023c4259ae354276773a3
riquiamado <carlosamado221471@gmail.com>
2023-03-17 22:31:12 -0300

    f54545

--- api/index.js
+++ api/index.js
@@ -19,10 +19,10 @@
 //     ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 const server = require("./src/app.js");
 const { conn } = require("./src/db.js");
-const PORT = process.env || 3001;
+const PORT = process.env.PORT || 3001;
 // Syncing all the models at once.
 conn.sync({ force: false }).then(() => {
-  server.listen(3001, () => {
+  server.listen(PORT, () => {
     console.log(`listening at${PORT}`); // eslint-disable-line no-console
   });
 });
