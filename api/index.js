95d88a84c563a60626f6bc03296e7e14b3d0286f
riquiamado <carlosamado221471@gmail.com>
2023-03-17 22:54:45 -0300

    f54545

--- api/index.js
+++ api/index.js
@@ -23,6 +23,6 @@ const PORT = process.env.PORT || 3001;
 // Syncing all the models at once.
 conn.sync({ force: false }).then(() => {
   server.listen(PORT, () => {
-    console.log(`listening at ${PORT}`); // eslint-disable-line no-console
+    console.log(`%s listening at ${PORT}`); // eslint-disable-line no-console
   });
 });
